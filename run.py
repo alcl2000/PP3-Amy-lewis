1adf7d4b68ccfdef9e87ca92e65fda4da4769490
alcl2000 <amy.555.lewis@gmail.com>
2022-10-21 12:36:29 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,2 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# import gspread
+# from google.oauth2.service_account import credentials
