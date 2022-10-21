153e075a871887acc878cb1e55dbb8cce191a170
alcl2000 <amy.555.lewis@gmail.com>
2022-10-21 16:46:22 +0000

    python: columns defined

--- run.py
+++ run.py
@@ -1,2 +1,23 @@
-# import gspread
-# from google.oauth2.service_account import credentials
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('hang-man-choices')
+OPTIONS = SHEET.worksheet('options')
+
+# Columns/Word selection
+columns = []
+for ind in range(0, 2):
+    column = OPTIONS.col_values(ind)
+    columns.append(column)
+easy_values = columns[0]
+medium_values = columns[1]
+hard_values = columns[2]
