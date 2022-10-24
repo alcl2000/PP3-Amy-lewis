42277b1eb83d56beae16c2b4adc43e61d638a44d
alcl2000 <amy.555.lewis@gmail.com>
2022-10-24 17:04:05 +0000

    python: pick_word function added

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import random
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -38,6 +39,7 @@ def start_game():
                 break
             else:
                 print('Please pick a valid number')
+                break
         except ValueError:
             print('Error: Please enter a number')
 
@@ -47,15 +49,36 @@ def choose_difficulty():
     picks the difficulty from one to three, and picks a word from the column
     """
     print('Select your difficulty\n 1. Easy 2. Medium 3. Hard')
-    mode = input('')
-    if mode == '1':
-        print('easy mode selected')
-    elif mode == '2':
-        print('medium mode selected')
-    elif mode == '3':
-        print('hard mode selected')
-    else:
-        print('please select a number')
+    while True:
+        mode = input('')
+        mode = int(mode)
+        try:
+            if mode == 1:
+                print('easy mode selected')
+                pick_word(1)
+            elif mode == 2:
+                print('medium mode selected')
+                pick_word(2)
+            elif mode == 3:
+                print('hard mode selected')
+                pick_word(3)
+            else:
+                print('Please select a difficulty ')
+        except ValueError:
+            print('Please enter a valid number')
+
+
+def pick_word(difficulty):
+    """
+    takes the value of choose_difficulty to select the word for hangman
+    """
+    choice = random.randrange(13)
+    if difficulty == 1:
+        print(easy_values(choice))
+    elif difficulty == 2:
+        print(medium_values(choice))
+    elif difficulty == 3:
+        print(hard_values(choice))
 
 
 start_game()
